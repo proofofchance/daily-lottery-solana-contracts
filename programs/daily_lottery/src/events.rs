bf9f5ec1d24bc28f49ed50ed21e83845e2d92687
Plexer44 <plexer44@users.noreply.github.com>
2026-04-24 19:50:55 +0100

    Emit upload window in lottery created event

--- programs/daily_lottery/src/events.rs
+++ programs/daily_lottery/src/events.rs
@@ -68,6 +68,8 @@ pub enum LotteryEvent {
         created_at_unix: i64,
         buy_start_unix: i64,
         buy_deadline_unix: i64,
+        upload_start_unix: i64,
+        upload_deadline_unix: i64,
     },
 
     /// Emitted when tickets are purchased
@@ -403,6 +405,8 @@ mod tests {
             created_at_unix: 1640995200,
             buy_start_unix: 1640995200,
             buy_deadline_unix: 1641081600,
+            upload_start_unix: 1641081600,
+            upload_deadline_unix: 1641168000,
         };
 
         let wrapper = EventWrapper {
